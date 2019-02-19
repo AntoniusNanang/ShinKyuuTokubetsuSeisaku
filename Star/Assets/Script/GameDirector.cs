498ed1d778cef41fbdb9fbf11ec311df1592c8e9
Ryuya0914 <tanaka.ryuya.0914@gmail.com>
2019-02-19 16:28:50 +0900

    反射衛星と恒星を作成

---
+++ Star/Assets/Script/GameDirector.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameDirector : MonoBehaviour {
+    public GameObject Sun;
+    public LayerMask layermask;
+    //光源のスタート方向
+    int StartDir = 6;
+    //光を飛ばしているオブジェクト
+    public List<GameObject> lightObj = new List<GameObject>();
+	
+	void Update () {
+        if (Input.GetMouseButtonDown(0) && !Input.GetMouseButton(2)) {
+            ReLight();
+        } else if (Input.GetMouseButtonDown(1) && !Input.GetMouseButton(2)) {
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+            if (Physics.Raycast(ray, out hit, 20.0f, layermask)) {
+                hit.collider.GetComponent<Satellite>().Spin();
+                ReLight();
+            }
+        }
+    }
+
+    void ReLight() {
+        for(int i = 0; i < lightObj.Count; i++) {
+            lightObj[i].GetComponent<Satellite>().Lightoff();
+        }
+        lightObj.Clear();
+        Sun.GetComponent<Satellite>().RayCast(StartDir, 4);
+    }
+}
