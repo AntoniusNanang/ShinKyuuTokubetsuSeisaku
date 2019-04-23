3bb3730bff380131c16aacf100b76e18c8f58f49
AntoniusNanang <nkcgta.antonius.nanang@gmail.com>
2019-04-23 13:45:51 +0900

    色々いじったが何も変えてない

---
+++ Star/Assets/Script/FadeSystem.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class FadeSystem : MonoBehaviour {
+
+    public Image FadeImage;
+    public string SceneName;
+
+    public static bool isFade;
+    public static bool isFadeIn;
+    public static bool isFadeOut;
+
+    float alpha;
+
+	// Use this for initialization
+	void Start () {
+
+        alpha = 1.0f;
+        isFade = true;
+        isFadeIn = true;
+        isFadeOut = false;
+
+        transform.SetAsFirstSibling();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (isFade)
+        {
+            if (isFadeIn)          FadeIn();
+            else if (isFadeOut)    FadeOut();
+        }
+	}
+
+    void FadeIn()
+    {
+        alpha -= 0.05f;
+        FadeImage.color = new Color(0.0f, 0.0f, 0.0f, alpha);
+        if(alpha <= 0.0f)
+        {
+            isFade = false;
+            isFadeIn = false;
+
+            transform.SetAsLastSibling();
+        }
+    }
+
+    void FadeOut()
+    {
+        alpha += 0.05f;
+        FadeImage.color = new Color(0.0f, 0.0f, 0.0f, alpha);
+        if(alpha >= 1.0f)
+        {
+            isFade = false;
+            isFadeOut = false;
+
+            //SceneManager.LoadScene(SceneName);
+        }
+    }
+}
