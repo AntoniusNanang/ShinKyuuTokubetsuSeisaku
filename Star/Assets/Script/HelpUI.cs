be3fbe47a377d4a7b1d30b2cc9ad46d2b89529f7
sho0630 <nkcgta.sho.takeuchi@gmail.com>
2019-05-14 11:22:31 +0900

    ステージセレクトUI

---
+++ Star/Assets/Script/HelpUI.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HelpUI : MonoBehaviour
+{
+
+    public GameObject help;
+    bool a = false;
+    void Start()
+    {}
+    void Update()
+    {}
+
+    public void HELP()
+    {
+        if (a == true)
+        {
+            help.SetActive(false);
+            a = false;
+        }
+        else if (a == false)
+        {
+            help.SetActive(true);
+            a = true;
+        }
+    }
+
+    public void Back()
+    {
+        help.SetActive(false);
+        a = false;
+    }
+}
+
