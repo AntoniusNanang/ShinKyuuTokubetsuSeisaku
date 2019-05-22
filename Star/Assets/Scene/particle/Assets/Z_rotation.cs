3b518186820c26408af3f76bc5b80c9637ae7b01
MatsuiKenta <47652401+iamark18game@users.noreply.github.com>
2019-05-22 15:42:02 +0900

    sceneにparticle名でtitleの背景を追加

---
+++ Star/Assets/Scene/particle/Assets/Z_rotation.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Z_rotation : MonoBehaviour
+{
+
+	// Use this for initialization
+	void Start ()
+    {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        //rotation Y を0～360まで回して360になったら0にする
+
+        {
+            transform.Rotate(new Vector3(0, 0, 0.05f));
+        }
+	}
+}
